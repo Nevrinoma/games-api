67b62d3ef14b0dcfa73d3c91d767326bd001c633
Opilane TTHK <opilane@tthk.ee>
2023-10-26 12:32:55 +0300

    As an app developer I want to get a full details of a given games to show in my app [Delivers #186290858]

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const app = require('express')()
 const port = 8080
 const swaggerUi = require ('swagger-ui-express')
-const swaggerDocument = require ('./docs/swagger.json')
+const yamljs = require  ('yamljs')
+const swaggerDocument = yamljs.load ('./docs/swagger.yaml');
+
+const games = [
+    {id: 1, name: "Witcher 3", price: 29.99},
+    {id: 2, name: "Cyberpunk 2077", price: 59.99},
+    {id: 3, name: "Minecraft", price: 26.99},
+    {id: 4, name: "Counter-Strike 2", price: 0},
+    {id: 5, name: "Roblox", price: 0},
+    {id: 6, name: "GTA V", price: 29.99},
+    {id: 7, name: "Valorant", price: 0},
+    {id: 8, name: "Forza Horizon 5", price: 59.99}
+];
 
 app.get('/games',(req,res) => {
-    res.send(["Withcer 3","Cyberpunk 2077","Dota 2"])
+    res.send(games)
+})
+
+app.get('/games/:id',(req,res) => {
+    if (typeof games[req.params.id - 1] === 'undefined'){
+        return res.status(404).send({error:"Game not found"})
+    }
+    res.send(games[req.params.id - 1])
 })
 
 
